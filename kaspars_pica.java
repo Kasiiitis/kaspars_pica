72e4b1a5dfe88e41535917750791fab976b14182
Kaspars <atliginskaspars@gmail.com>
2021-03-05 11:34:31 +0200

    Picas cenas-izmēri

--- kaspars_pica.java
+++ kaspars_pica.java
@@ -14,6 +14,22 @@ public class kaspars_pica {
 		adrese = JOptionPane.showInputDialog("ievadiet savu pasreizejo adresi");
 		JOptionPane.showMessageDialog(null, "Ðî ir jûsu adrese (pârbaude)?  " +adrese, adrese, JOptionPane.WARNING_MESSAGE);
 		TelefonaNummurs = JOptionPane.showInputDialog("ievadiet savu telefona nummuru, lai mçs varçtu paziòot sîkâku informaciju par jûsu pasûtijumu");
+		JOptionPane.showMessageDialog(null, "Ðis ir jûsu telefona nummurs (pârbaude)?  " +TelefonaNummurs, TelefonaNummurs, JOptionPane.WARNING_MESSAGE);
+		parbaude = TelefonaNummurs = JOptionPane.showInputDialog("ievadiet JA vai NE (Ja jûsu telefona nummurs atbilst vai neatbilst) ");
+		JOptionPane.showMessageDialog(null, "Jûsu nummurs tika piefiksçts" +parbaude, parbaude, JOptionPane.WARNING_MESSAGE);
+		
+		int picasIzmer;
+		double izmeraCena= 0;
+		String[] option2s = new String[] {"10,20,30,50"};
+		String picasIzmerStr = (String) JOptionPane.showInputDialog(null, "Izvçlieties cik lielu picu jûs vçlaties! 10 - 4.99, 20 - 9.99, 30 - 13.99, 50 - 17.99 ");
+		picasIzmer = Integer.parseInt(picasIzmerStr);
+		System.out.println(picasIzmer);
+		switch(picasIzmer) {
+		case 10: izmeraCena = 4.99; break;
+		case 20: izmeraCena = 9.99; break;
+		case 30: izmeraCena = 13.99; break;
+		case 50: izmeraCena = 17.99; break;
+		}
 	}
 
 }
