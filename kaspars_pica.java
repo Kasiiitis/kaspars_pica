26b7e564e842c38436262291692b0bccca1842d5
Kaspars <atliginskaspars@gmail.com>
2021-03-05 11:10:14 +0200

    nodefinēju mainīgos

---
+++ kaspars_pica.java
@@ -0,0 +1,13 @@
+package kaspars_pica;
+
+public class kaspars_pica {
+
+	public static void main(String[] args) {
+		String adrese;
+		String vards;
+		String TelefonaNummurs;
+		String parbaude;
+
+	}
+
+}
